e263022717b64e19404a64bebac7f71b1f2fb341
ST0619 <stng0301@gmail.com>
2022-10-13 21:05:33 +0800

    first lab

---
+++ Lab01.cpp
@@ -0,0 +1,22 @@
+//This is my first lab
+//13-10-2022 about c++
+//Author: NG SUE TING
+
+//or
+
+/*This is my first lab
+13-10-2022 about c++
+Author: NG SUE TING
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello \t World\n"; //this is one instruction
+	cout << "My name is NG SUE TING.\a\n";
+	cout << "My name is NG SUE TING.\n"<<endl;
+}
+
+//end of program
